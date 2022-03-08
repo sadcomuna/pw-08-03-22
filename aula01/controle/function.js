d16441c2622ad488db0b214835d6956b59a276da
sadcomuna <annalauraamorim063@gmail.com>
2022-03-08 12:10:42 -0300

    anna

--- aula01/controle/function.js
+++ aula01/controle/function.js
@@ -6,13 +6,22 @@ $(document).ready(function() {
 
         let dados = $('#form').serialize()
 
+        $('#retorno').empty()
+
         $.ajax({
             type: 'POST',
             dataType: 'JSON',
             assync: true,
             data: dados,
             url: '../modelo/function.php',
-            success: function(){
+            success: function(dados){
+                $('#retorno').append(` <div class="col-12 col-sm-8 col-md-6">
+                <div class="alert-primary">
+                    <h1 class="text-center text-dark"> ${dados.mensagem} </h1>
+
+                </div>
+                <img src="../../img/${dados.tipo}" class="img-fluid">
+            </div>`)
 
             }
         })
