02b52b81fe51b726d548126debfc63f74f944f7f
sadcomuna <annalauraamorim063@gmail.com>
2022-03-08 11:38:31 -0300

    anna

---
+++ aula01/modelo/retorno.php
@@ -0,0 +1,35 @@
+<?php
+
+    $NOME = $_REQUEST['NOME'];
+    $ANIMAL = $_REQUEST['ANIMAL'];
+
+    if(empty($NOME)){
+        
+        $dados = array(
+            "tipo" => 'error',
+            "mensagem" => 'Existem(m) campo(s) obrigatório(s) não preenchido(s).'
+        );
+    
+    } else {
+
+        switch($ANIMAL){
+            case '1' : $dados = array(
+                "tipo" => 'cachorro.jpg',
+                "mensagem" => 'Olá '.$NOME.', sabemos que seu animal preferido é o cachorro'
+            );
+            break;
+            case '2' : $dados = array(
+                "tipo" => 'gato.jpg',
+                "mensagem" => 'Olá '.$NOME.', sabemos que seu animal preferido é o gato'
+            );
+            break;
+            case '3' : $dados = array(
+                "tipo" => 'peixe.jpg',
+                "mensagem" => 'Olá '.$NOME.', sabemos que seu animal preferido é o peixe'
+            );
+            break;
+        }
+
+    }
+
+    echo json_encode($dados);
